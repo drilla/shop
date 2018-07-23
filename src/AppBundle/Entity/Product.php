fb450178cf61fb2dd427a65c004a905c4fbb8e15
drilla <drillerok@gmail.com>
2018-07-23 23:30:03 +0300

    product

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Product
+{
+
+    private $id;
+
+    /** @var string */
+    private $name;
+
+    /** @var float */
+    private $price;
+
+    /** @var string */
+    private $description;
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName(string $name) : self {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getPrice() : float {
+        return $this->price;
+    }
+
+    public function setPrice(float $price) : self {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function getDescription() : string {
+        return $this->description;
+    }
+
+    public function setDescription(string $description) : self {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getId() : int {
+        return $this->id;
+    }
+}
