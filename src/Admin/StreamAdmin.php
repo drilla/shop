e181c5518b35f0347b7f370644c908bc7009cb90
vkuznetsov <vkuznetsov@zdorov.pro>
2018-08-14 11:55:33 +0300

    stream

---
+++ src/Admin/StreamAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+/**
+ * @author drilla
+ */
+class StreamAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper) : void {
+        $formMapper
+            ->add('streamId', TextType::class)
+            ->add('product', ModelType::class, ['property' => 'name'])
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $dataGridMapper) : void {
+        $dataGridMapper
+            ->add('product', 'doctrine_orm_model_autocomplete' , [], null, ['property'=>'title'] )
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper) : void {
+        $listMapper
+            ->addIdentifier('product', null, ['associated_property' => 'name'])
+            ->addIdentifier('streamId')
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Stream.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * @author drilla
+ */
+class Stream
+{
+    private $id;
+    private $streamId;
+    private $product;
+
+    public function getId() : int {
+        return $this->id;
+    }
+
+    public function setId(int $id) : Stream {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getStreamId() : ? int {
+        return $this->streamId;
+    }
+
+    public function setStreamId(string $streamId) : Stream {
+        $this->streamId = $streamId;
+        return $this;
+    }
+
+    public function getProduct() : ? Product {
+        return $this->product;
+    }
+
+    public function setProduct(int $product) : Stream {
+        $this->product = $product;
+        return $this;
+    }
+}
